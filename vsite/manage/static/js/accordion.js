63dddd9cb763edb27445147d65a32bdfd2d8e3a6
Zeng Ying <vuuvv@qq.com>
2012-05-24 17:47:52 +0800

    accordion done

--- vsite/manage/static/js/accordion.js
+++ vsite/manage/static/js/accordion.js
@@ -1,78 +1,90 @@
-YUI.add('accordion', function(Y) {
+!function($) {
 
-var Lang = Y.Lang,
-	Node = Y.Node,
-	Anim = Y.Anim,
-	Easing = Y.Easing,
-	WidgetStdMod = Y.WidgetStdMod,
-	AccName = "accordion",
-	AccItemName = "accordion-item",
-	getCN = Y.ClassNameManager.getClassName,
+"use strict"; // jshint ;_;
 
-	C_ITEM = "yui3-accordion-item";
+var Accordion = function(element, options) {
+	var $element = this.$element = $(element),
+		heads_length = 0;
+	this.options = $.extend({}, $.fn.accordion.defaults, options);
+	if (!$element.hasClass('accordion'))
+		$element.addClass('accordion');
+	this.$groups = $element.find('.accordion-group').each(function(index, elem) {
+		var $e = $(elem);
+		$e.data('index', index).addClass("static");
+		heads_length += $e.height();
+	});
+	this.heads_length = heads_length;
+	this.show($element.find('>.accordion-group:first-child').data('index'));
+};
 
-Y.Accordion = Y.Base.create(AccName, Y.Widget, [], {
-	initializer: function(config) {
-		console.log("initializer");
+Accordion.prototype = {
+	constructor: Accordion,
+
+	_body_height: function() {
+		return this.$element.height() - this.heads_length;
 	},
 
-	destructor: function() {
-		console.log("initializer");
-	},
+	show: function(index) {
+		var group = $(this.$groups[index]),
+			active = this.$element.find('> .accordion-group.shown'),
+			head = group.find(".accordion-heading"),
+			body = group.find(".accordion-body");
 
-	renderUI: function() {
-		var srcNode, itemsDom;
-		srcNode = this.get("srcNode");
+		if (group.hasClass("shown"))
+			return;
 
-		itemsDom = srcNode.all("> ." + C_ITEM);
-		itemsDom.each(function(itemNode, index, itemsDom) {
-			var newItem;
-
-			newItem = new Y.AccordionItem({
-				srcNode: itemNode,
-				id: itemNode.get("id")
+		if (active) {
+			active.removeClass("shown");
+			active.removeClass("static");
+			active.find(".accordion-body").animate({height: 0}, function(){
+				active.addClass("static");
 			});
-			this.addItem(newItem);
-		}, this);
-	},
-
-	bindUI: function() {
-		console.log("bind ui");
-	},
-
-	syncUI: function() {
-		console.log("sync ui");
-	},
-
-	addItem: function(item) {
-		var items = this.get("items");
-		items.push(item);
-	}
-}, {
-	NAME: AccName,
-
-	ATTRS: {
-		items: {
-			value: [],
-			readOnly: true,
-			validator: Lang.isArray
 		}
+
+		group.addClass("shown");
+		group.removeClass("static");
+		body.animate({
+			height: this._body_height()
+		}, function() {
+			group.addClass("static");
+		});
+	},
+
+	_show_body: function(body) {
+	},
+
+	toggle: function(index) {
+		this.show(index);
 	}
+};
+
+/* PLUGIN DEFINITION */
+$.fn.accordion  = function(option) {
+	var args = arguments;
+	return this.each(function() {
+		var $this = $(this),
+			data = $this.data('accordion'),
+			options = typeof option == 'object' && option;
+		if (!data) 
+			$this.data('accordion', (data = new Accordion(this, options)));
+		if (typeof option == 'string') 
+			data[option].apply(data, Array.prototype.slice.call(args, 1));
+	});
+};
+
+$.fn.accordion.defaults = {
+	toggle: true
+};
+
+$.fn.accordion.Constructor =  Accordion;
+
+$(function() {
+	$('body').on('click.accordion.data-api', '[data-toggle=accordion]', function(e) {
+		var $this = $(this),
+			$target = $this.parents('.accordion');
+		e.preventDefault();
+		$target.accordion('toggle', $this.parents('.accordion-group').data('index'));
+	});
 });
 
-Y.AccordionItem = Y.Base.create(AccItemName, Y.Widget, [WidgetStdMod], {
-	initializer: function(config) {
-	},
-
-	destructor: function() {
-	},
-
-	renderUI: function() {
-	},
-
-	bindUI: function() {
-	}
-
-});
-
-}, '1.0', {requires: ['widget', 'widget-stdmod']});
+}(jQuery);
