d93981282cd19aa6dc4b44c4d36277a9fdd34071
vuuvv <vuuvv@qq.com>
2012-07-03 01:35:22 +0800

    menu

--- vsite/manage/static/js/manage/views/app.js
+++ vsite/manage/static/js/manage/views/app.js
@@ -5,8 +5,23 @@ define([
 	'text!templates/app.html'
 ], function($, _, Backbone, app_template) {
 	var AppView = Backbone.View.extend({
+		el: $("body"),
+
+		events: {
+			"mouseover #global-header .menu li.menu-item": "mouse_in_menu",
+			"mouseout #global-header .menu li.menu-item": "mouse_out_menu"
+		},
+
 		initialize: function() {
 			$("#global-header").width($("#main").width());
+		},
+
+		mouse_in_menu: function(e) {
+			$(e.target).parent("li").addClass("current");
+		},
+
+		mouse_out_menu: function(e) {
+			$(e.target).parent("li").removeClass("current");
 		}
 	});
 	return AppView;
