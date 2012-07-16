73b9e3dd1319b5f2af39c02e5485d9d70e6eaa62
sargerasy <sargerasy@gmail.com>
2012-07-16 17:40:00 +0800

    fix

---
+++ site_demo/site_demo/allin/js/main.js
@@ -0,0 +1,15 @@
+
+$(function() {
+	function fBodyVericalAlign(){
+		var nBodyHeight = 572;
+		var nClientHeight = document.documentElement.clientHeight;
+		if(nClientHeight >= nBodyHeight + 2){
+			var nDis = (nClientHeight - nBodyHeight)/2;
+			document.body.style.paddingTop = nDis + 'px';
+		}else{
+			document.body.style.paddingTop = '0px';
+		}
+	} 
+	fBodyVericalAlign();
+	$(window).bind("resize", fBodyVericalAlign);
+});
