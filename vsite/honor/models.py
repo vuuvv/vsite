cbacfe0c2914de127a7221d58f57bec35be003b2
unknown <yxzxzy@.joyou.com>
2012-10-15 16:47:57 +0800

    ddd

--- vsite/honor/models.py
+++ vsite/honor/models.py
@@ -3,18 +3,24 @@ from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 
 HONOR_ROOT = settings.UPLOAD_ROOT + 'honor/'
+HONOR_YEAR = settings.UPLOAD_ROOT + 'year/'
 HONOR_THUMB_ROOT = HONOR_ROOT + 'thumb/'
 
 class Category(models.Model):
 	name = models.CharField(_("Category"), max_length=50)
 	slug = models.CharField(_("slug"), max_length=50, blank=True)
 	ordering = models.IntegerField(_("Ordering"), default=1000) 
+	image = models.ImageField(_("Image"), upload_to=HONOR_YEAR, null=True, blank=True)
 
 	class Meta:
 		ordering = ("ordering", "-name",)
 
 	def __unicode__(self):
 		return self.name
+
+	@models.permalink
+	def get_absolute_url(self):
+		return ('honor_list', (self.slug,), {})
 
 	def save(self, *args, **kwargs):
 		if self.slug is None:
@@ -27,7 +33,7 @@ class Honor(models.Model):
 	description = models.TextField(_("Description"), blank=True)
 	image = models.ImageField(_("Image"), upload_to=HONOR_ROOT, blank=True)
 	thumbnail = models.ImageField(_("Thumbnail"), upload_to=HONOR_THUMB_ROOT, blank=True)
-	ordering = models.IntegerField(_("Ordering"), default=1000) 
+	ordering = models.IntegerField(_("Ordering"), default=1000)
 	active = models.BooleanField(_("Active"), default=True)
 
 	class Meta:
--- vsite/honor/views.py
+++ vsite/honor/views.py
@@ -5,11 +5,19 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Category, Honor
 from vsite.pages.middleware import get_page_context
+from vsite.utils import chunks
 
 def honor_list(request, slug=None, template="honor/index.html", extra_context=None):
-	honors = Honor.objects.all()
-	context = get_page_context("/about/honor/")
-	context["honors"] = honors
-	extra_context.update(context)
-	return TemplateResponse(request, template, extra_context)
+    category = Category.objects.all()
+    context = get_page_context("/about/honor/")
+    context["years"] = category
+    extra_context.update(context)
+    return TemplateResponse(request, template, extra_context)
+
+def honor_year(request, slug=None, template="honor/year.html", extra_context=None):
+    category = Category.objects.get(slug=slug)
+    context = get_page_context("/about/honor/")
+    context["honors"] = category.honors
+    extra_context.update(context)
+    return TemplateResponse(request, template, extra_context)
 
